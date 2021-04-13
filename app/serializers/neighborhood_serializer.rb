7582972b24ef7eed5ac610b499f284e644fe29b5
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-13 16:49:24 -0500

    Added serializers for all models

---
+++ app/serializers/neighborhood_serializer.rb
@@ -0,0 +1,4 @@
+class NeighborhoodSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
---
+++ app/serializers/rooftop_serializer.rb
@@ -0,0 +1,4 @@
+class RooftopSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
