4cc3bbab5c8fb2d7adaba8465d975912e9cdfae4
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-15 15:10:08 -0500

    Added user controller actions, validations, and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :rooftops
   has_secure_password
 
+  validates :name, :username, :password, presence: true
+  validates :username, uniqueness: { case_sensitive: false }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do  
       resources :rooftops, only: [:index, :create]
       resources :neighborhoods, only: [:index]
+      resources :users, only: [:index, :create]
     end
   end
 end
