87ae39b2950b6cccdc200311b6ced9fed1910b20
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-15 15:44:22 -0500

    Skip authorization for rooftop index

--- app/controllers/api/v1/rooftops_controller.rb
+++ app/controllers/api/v1/rooftops_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RooftopsController < ApplicationController 
 
-  skip_before_action :authorized, only: [:index]
+  skip_before_action :authorized, only: [:index, :home]
 
   def home
     redirect_to api_v1_rooftops_path
