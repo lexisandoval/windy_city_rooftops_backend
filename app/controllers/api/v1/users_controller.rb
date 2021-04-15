b8771e5945948a72a38a9514f068809be5134c4f
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-15 15:47:58 -0500

    Added create action whicch utilizes jwt to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController 
 
+  skip_before_action :authorized, only: [:create]
+
   def index
     users = User.all
     render json: UserSerializer.new(users)
