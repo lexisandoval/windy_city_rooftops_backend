0af327d4f9490c4a9bae944bbb44480cf91a3cc2
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-15 16:15:26 -0500

    Deleted user model

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-
-  has_many :rooftops
-  has_secure_password
-
-  validates :name, :username, :password, presence: true
-  validates :username, uniqueness: { case_sensitive: false }
-
-end
