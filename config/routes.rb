f4a7b1517c52299be6cdb22d1e8b231e1c1f5129
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-19 15:07:09 -0500

    Added route for update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do  
-      resources :rooftops, only: [:index, :create]
+      resources :rooftops, only: [:index, :create, :update]
       resources :neighborhoods, only: [:index]
     end
   end
