558268af13827b2d90920f4508365d49cf770852
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-19 19:45:48 -0500

    Added additional validation

--- app/models/rooftop.rb
+++ app/models/rooftop.rb
@@ -2,4 +2,5 @@ class Rooftop < ApplicationRecord
   belongs_to :neighborhood
 
   validates :name, :address, :image_url, :website_url, :description, presence: true
+  validates :name, uniqueness: true
 end
